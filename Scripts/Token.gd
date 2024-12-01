c6b3866450367ea1554febf70f07005cc65d9acf
Kaku <a603938017@gmail.com>
2024-12-01 21:13:18 +0900

    feat: right click to show the neighbor and calc score

--- Scripts/Token.gd
+++ Scripts/Token.gd
@@ -3,6 +3,8 @@ class_name CToken
 static  var max_id = 0
 var id: int
 var floating_score_text
+var curTokens
+var curIndex
 
 @export var tokenName: String = "Default Text"
 @export var level: int = 0
@@ -19,6 +21,7 @@ func _ready():
 	#pass # Replace with function body.
 	$Label.text = tokenName
 	$Level.text = str(level)
+	mouse_filter = Control.MOUSE_FILTER_STOP
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -43,8 +46,16 @@ func set_token_name(_name):
 
 func toString():
 	return "[id:" + str(id) + ", name:" + tokenName + ",level:" + str(level) + "]"
-	
-func calc_score(index: int, tokens: Array[CToken], rowNum: int, colNum: int):
+
+func change_color_temporary(color):
+	var colorRectNode = get_node("ColorRect")
+	colorRectNode.color = color
+	await get_tree().create_timer(3).timeout
+	colorRectNode.color = Color("#C56297")
+
+func calc_score(index: int, tokens: Array[CToken], rowNum: int, colNum: int, redundant: bool = false):
+	curTokens = tokens
+	curIndex = index
 	var offsets_8 = [
 		Vector2(-1, -1), Vector2(-1, 0), Vector2(-1, 1),
 		Vector2(0, -1), Vector2(0, 1),
@@ -55,6 +66,9 @@ func calc_score(index: int, tokens: Array[CToken], rowNum: int, colNum: int):
 	var coordinates = CToken.getCoordinateByIndex(index, rowNum, colNum)
 	var row = coordinates[0]
 	var col = coordinates[1]
+	
+	if redundant:
+		print("current token pos:" + str(row) + ", " + str(col))
 	#var offsets_4 = [
 		#Vector2(-1, 0),
 		#Vector2(0, -1), Vector2(0, 1),
@@ -68,13 +82,19 @@ func calc_score(index: int, tokens: Array[CToken], rowNum: int, colNum: int):
 		# Check edge
 		if neighbor_row >= 0 and neighbor_row < rowNum and neighbor_col >= 0 and neighbor_col < colNum:
 			var neighbor_index = neighbor_row * colNum + neighbor_col
-			var neghbor_token = tokens[neighbor_index]
-			if neghbor_token.tokenName == tokenName:
-				score += 1
+			var neighbor_token = tokens[neighbor_index]
+			var colorRectNode = neighbor_token.get_node("ColorRect")
+			if neighbor_token.tokenName == tokenName:
+				score *= 2
+				if redundant:
+					neighbor_token.change_color_temporary(Color.BISQUE)
+			else:
+				if redundant:
+					print("neighbor token pos:" + str(neighbor_row) + ", " + str(neighbor_col))
+					neighbor_token.change_color_temporary(Color.AQUA)
 			neighbors.append(neighbor_index)
 
 	return score
-	
 	
 func show_score(value: int, pos: Vector2 = Vector2(10,10)) -> void:
 	if !pos:
@@ -96,3 +116,10 @@ func reset():
 	if floating_score_text != null:
 		floating_score_text.queue_free()
 		floating_score_text = null
+
+func _on_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+			print("on token gui input", curIndex, curTokens)
+			var score = calc_score(curIndex, curTokens, 5, 5, true)
+			show_score(score)
